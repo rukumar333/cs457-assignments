c835bca45ab0a8100587a94cbe6862c2b300c9d7
Rushil Kumar <rukumar333@gmail.com>
2018-03-04 21:36:06 -0500

    Basic client and server.

---
+++ Assignment_1/client.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import glob
+import hashlib
+import sys
+sys.path.append('gen-py')
+
+from chord import FileStore
+from chord.ttypes import SystemException, RFileMetadata, RFile, NodeID
+
+from thrift import Thrift
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+
+def main():
+    # Make socket
+    transport = TSocket.TSocket('localhost', 9090)
+
+    # Buffering is critical. Raw sockets are very slow
+    transport = TTransport.TBufferedTransport(transport)
+
+    # Wrap in a protocol
+    protocol = TBinaryProtocol.TBinaryProtocol(transport)
+
+    # Create a client to use the protocol encoder
+    client = FileStore.Client(protocol)
+
+    # Connect!
+    transport.open()
+
+    rFile = RFile()
+    meta_data = RFileMetadata()
+    meta_data.filename = 'test.txt'
+    meta_data.version = 0
+    meta_data.owner = 'rushil'
+    hash_input = meta_data.owner + ':' + meta_data.filename
+    hash_input = hash_input.encode('utf-8')
+    meta_data.contentHash = hashlib.sha256(hash_input).hexdigest()
+    rFile.content = 'Hello world!'
+    rFile.meta = meta_data
+    client.writeFile(rFile)
+
+    read_rFile = client.readFile('test.txt', 'rushil')
+
+    # Close!
+    transport.close()
+
+if __name__ == '__main__':
+    main()
--- Assignment_1/server.py
+++ Assignment_1/server.py
@@ -1,16 +1,69 @@
 #!/usr/bin/env python
 
 import glob
+import hashlib
+from pathlib import Path
 import sys
 sys.path.append('gen-py')
 
 from chord import FileStore
-from chord import SystemException, RFileMetadata, RFile, NodeID
+from chord.ttypes import SystemException, RFileMetadata, RFile, NodeID
 
 from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 from thrift.server import TServer
 
+class FileStoreHandler:
+    def __init__(self):
+        self.log = {}
+        self.files = {}
+
+    def writeFile(self, rFile):
+        print('writeFile')
+        print('Content: {}'.format(rFile.content))
+        self.files[rFile.meta.contentHash] = rFile.meta
+        server_file = Path(rFile.meta.owner + '_' + rFile.meta.filename)
+        if server_file.is_file():
+            self.files[rFile.meta.version] += 1
+        server_file.write_text(rFile.content)
+            
+
+    def readFile(self, filename, owner):
+        print('readFile')
+        hash_input = owner + ':' filename
+        hash_input = hash_input.encode('utf-8')
+        file_hash = hashlib.sha256(file_hash).hexdigest()
+        if file_hash in self.files:
+            rFile = RFile()
+            rFile.content = Path(owner + '_' + filename).read_text()
+            rFile.meta = self.files[file_hash]
+            return rFile
+        else:
+            exception = SystemException()
+            exception.message = 'File {} owned by {} does not exist!'.format(filename, owner)
+            raise exception
+
+    def setFingerTable(self, node_list):
+        print('setFingerTable')
+
+    def findSucc(self, key):
+        print('findSucc')
+
+    def findPref(self, key):
+        print('findPref')
+
+    def getNodeSucc(self):
+        print('getNodeSucc')
+
 if __name__ == '__main__':
+    handler = FileStoreHandler()
+    processor = FileStore.Processor(handler)
+    transport = TSocket.TServerSocket(port=9090)
+    tfactory = TTransport.TBufferedTransportFactory()
+    pfactory = TBinaryProtocol.TBinaryProtocolFactory()
+
     print('Starting server')
+    server = TServer.TSimpleServer(processor, transport, tfactory, pfactory)
+    server.serve()
+    print('Ended server')
