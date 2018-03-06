f3e1d79ca5a33debff1e3fcc391dc88380b3a73b
Rushil Kumar <rukumar333@gmail.com>
2018-03-06 10:55:02 -0500

    Implemented findSucc but need to check for self RPC. Writing output to log files.

--- Assignment_1/server.py
+++ Assignment_1/server.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
-
 import glob
 import hashlib
-# from pathlib import Path
-import os
+import logging
 import sys
 sys.path.append('gen-py')
 # sys.path.insert(0, glob.glob('/home/yaoliu/src_code/local/lib/lib/python2.7/site-packages/')[0])
@@ -15,6 +13,8 @@ from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 from thrift.server import TServer
+
+logging.basicConfig(level=logging.DEBUG)
 
 class FileStoreHandler:
     def __init__(self):
@@ -34,8 +34,6 @@ class FileStoreHandler:
         file_name = rFile.meta.owner + '_' + rFile.meta.filename
         with open(file_name, 'w') as server_file:
             server_file.write(rFile.content)
-        # server_file = Path(rFile.meta.owner + '_' + rFile.meta.filename)
-        # server_file.write_text(rFile.content)
 
     def readFile(self, filename, owner):
         print('readFile')
@@ -60,22 +58,76 @@ class FileStoreHandler:
         print('setFingertable')
         self.finger_table = node_list
         print('Length of fingertable: {}'.format(len(node_list)))
-        # print('Inputed finger table:')
-        # print(node_list)
+        print(self.finger_table)
 
     def findSucc(self, key):
         print('findSucc')
+        predecessor_node = self.findPred(key)
+        print('Predecessor node found:')
+        print(predecessor_node)
+        if predecessor_node is not None: # Current node is predeccesor
+            transport = TSocket.TSocket(predecessor_node.ip, predecessor_node.port)
+            transport = TTransport.TBufferedTransport(transport)
+            protocol = TBinaryProtocol.TBinaryProtocol(transport)
+            client = FileStore.Client(protocol)
+            transport.open()
+            owner_node = client.getNodeSucc()
+            transport.close()
+            return owner_node
+        else: # Another node is predecessor
+            owner_node = self.getNodeSucc()
+            return owner_node
 
-    def findPref(self, key):
-        print('findPref')
+    def findPred(self, key):
+        print('findPred')
+        next_node = None
+        key_num = int(key, 16)
+        if self.finger_table:
+            if int(self.finger_table[0].id, 16) > key_num:
+                # Means current node is predecessor
+                return None
+            for node in self.finger_table:
+                if next_node is not None and (int(node.id, 16) > key_num or \
+                   int(next_node.id, 16) > int(node.id, 16)):
+                    # Current node succeeds key or previous node succeeds current node
+                    break
+                next_node = node
+            assert next_node is not None
+            print('Next node port: {}'.format(next_node.port))
+            transport = TSocket.TSocket(next_node.ip, next_node.port)
+            transport = TTransport.TBufferedTransport(transport)
+            protocol = TBinaryProtocol.TBinaryProtocol(transport)
+            client = FileStore.Client(protocol)
+            transport.open()
+            predecessor_node = client.findPred(key)
+            transport.close()
+            if predecessor_node == None:
+                return next_node
+            else:
+                return predecessor_node
+        else:
+            exception = SystemException()
+            exception.message = 'Node does not have a fingertable!'
+            raise exception
 
     def getNodeSucc(self):
         print('getNodeSucc')
+        if self.finger_table:
+            return self.finger_table[0]
+        else:
+            exception = SystemException()
+            exception.message = 'Node does not have a fingertable!'
+            raise exception
 
 if __name__ == '__main__':
     port_num = 9090
     if len(sys.argv) > 1:
         port_num = int(sys.argv[1])
+
+    orig_stdout = sys.stdout
+    file_stdout = open('log_' + str(port_num) + '.txt', 'w', 0)
+    sys.stdout = file_stdout
+
     handler = FileStoreHandler()
     processor = FileStore.Processor(handler)
     transport = TSocket.TServerSocket(port=port_num)
@@ -86,3 +138,4 @@ if __name__ == '__main__':
     server = TServer.TSimpleServer(processor, transport, tfactory, pfactory)
     server.serve()
     print('Ended server')
+    file_stdout.close()
