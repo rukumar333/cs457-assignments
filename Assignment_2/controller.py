92625b930e868f21ef63a4af788e9cf2aa71758f
Rushil Kumar <rukumar333@gmail.com>
2018-03-23 02:30:07 -0400

    More rigorous testing of bank.

--- Assignment_2/controller.py
+++ Assignment_2/controller.py
@@ -20,6 +20,11 @@ import bank_pb2
 snapshot_id = 0
 sockets = []
 
+def get_branch_index(name):
+    for i in range(len(sockets)):
+        if name == sockets[i][1]:
+            return i
+
 def message_socket(sock, message):
     # print('Messaging socket')
     message_string = message.SerializeToString()
@@ -46,7 +51,7 @@ def initialize_bank(money, branches_file):
             print(branch.port)
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             sock.connect((branch.ip, branch.port))
-            sockets.append((sock, branch.port))
+            sockets.append((sock, branch.name))
             message_socket(sock, message)
 
 def initialize_sockets(branches_file):
@@ -61,7 +66,7 @@ def initialize_sockets(branches_file):
             print(port)
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             sock.connect((ip, port))
-            sockets.append((sock, port))
+            sockets.append((sock, name))
 
 def initialize_snapshot(snapshot_id):
     init = bank_pb2.InitSnapshot()
@@ -81,8 +86,11 @@ def get_snapshot(snapshot_id, connections, money):
     total = 0
     print('snapshot_id: {}'.format(snapshot_id))
     for sock in connections:
-        print('Port: {}'.format(sock[1]))
+        result_str = ''
+        # print('Port: {}'.format(sock[1]))
+        # Message server
         message_socket(sock[0], message)
+        # Read returned snapshot
         initial_data = sock[0].recv(2)
         data_size = struct.unpack('H', initial_data)[0]
         data = sock[0].recv(data_size)
@@ -90,12 +98,23 @@ def get_snapshot(snapshot_id, connections, money):
         rec_message.ParseFromString(data)
         assert rec_message.WhichOneof('branch_message') == 'return_snapshot'
         local_snapshot = rec_message.return_snapshot.local_snapshot
-        print('Balance: {}'.format(local_snapshot.balance))
-        print('channel_states: ')
-        print(local_snapshot.channel_state)
+        # Get initial balance
+        result_str += '{}: {}, '.format(sock[1], local_snapshot.balance)
+        # print('{}: {}'.format(sock[1], local_snapshot.balance))
+        if not all(x == 0 for x in local_snapshot.channel_state):
+            # print()
+            print('channel_states: ')
+            print(local_snapshot.channel_state)
         total += local_snapshot.balance
-        for amt in local_snapshot.channel_state:
-            total += amt
+        for i in range(len(local_snapshot.channel_state)):
+            if i != get_branch_index(sock[1]):
+                amt = local_snapshot.channel_state[i]
+                total += amt
+                result_str += '{}->{}: {}, '.format(connections[i][1], sock[1], amt)
+        # for amt in local_snapshot.channel_state:
+        #     result_str += '{}->{}: {}, '.format(, sock[1], amt)
+        #     total += amt
+        print(result_str)
     print('Total: {}'.format(total))
     print('')
     assert total == money
@@ -109,16 +128,20 @@ if __name__ == '__main__':
         print('Need more arguments')
         sys.exit(-1)
     else:
+        offset = 0
+        consec_reqs = 100
         print(snapshot_id)
         init_amt = int(sys.argv[1])
         initialize_bank(init_amt, sys.argv[2])
         # initialize_sockets(sys.argv[2])
-        time.sleep(3)
-        for x in range(200):
-            initialize_snapshot(x)
-        time.sleep(10)
-        for x in range(200):
-            get_snapshot(x, sockets, init_amt)
+        while offset < 10000:
+            time.sleep(5)
+            for x in range(consec_reqs):
+                initialize_snapshot(x + offset)
+            time.sleep(25)
+            for x in range(consec_reqs):
+                get_snapshot(x + offset, sockets, init_amt)
+            offset += consec_reqs
         # while snapshot_id < 50:
         #     initialize_snapshot(snapshot_id)
         #     time.sleep(3)
